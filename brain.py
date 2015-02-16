8d9f796868395443e0cdd060e17640d356409f4d
zws3mb <zws3mb@virginia.edu>
2015-02-16 18:11:32 -0500

    limited evaluation

--- brain.py
+++ brain.py
@@ -5,6 +5,8 @@ class brain:
         self.known_table={}
         self.working_mem=[]
         self.order_q=[]
+        self.op_dict={'&':'and','|':'or','!':'not'}
+        self.thought=''
     def teach(self,instring):
         if '=' in instring and instring[2] !='True' and instring[2] !='False':
             self.var_map[instring[0]]=instring[2]
@@ -31,7 +33,50 @@ class brain:
                 pass
         return instring
     def query(self,instring):
-        return str(instring)
+        self.thought=''
+        current=instring[0]
+        stack=[]
+        self.evaluate_tree(current)
+
+        return 'I THINK:'+str(eval(self.thought))
+    def convert(self,instr):
+        if instr in self.op_dict:
+            return self.op_dict[instr]
+        elif instr in self.known_table:
+            return self.known_table[instr]
+        elif instr in self.var_map:
+            return self.var_map[instr]
+        elif instr in self.working_mem:
+            return self.working_mem[instr]
+    def gather(self,val):
+        self.thought+=' '+val
+    def inorder(self,node,exp):
+        if node is not None:
+            self.inorder(node.left, exp)
+            self.gather(str(self.convert(node.value)))
+            self.inorder(node.right, exp)
+    def evaluate_tree(self,ptree):
+        exp=''
+        current=ptree
+        stack=[]
+        self.inorder(ptree,exp)
+        #
+        # while(current.value!=None):
+        #
+        #     # stack.append(current)
+        #     if current.left !=None:
+        #         stack.append(current.left)
+        #         #print current.left
+        #     exp+=' '+str(self.convert(current.value))
+        #     if current.right !=None:
+        #         stack.append(current.right)
+        #         #print current.right
+        #     if len(stack)>0:
+        #         current=stack.pop()
+        #     else:
+        #         break
+        print self.thought
+
     def why(self,instring):
         return instring
     def express(self,instring):
