189a767ecd4846dfa9b82e7f3daffcbc3f246572
zws3mb <zws3mb@virginia.edu>
2015-02-16 22:15:48 -0500

    fixed rule bug

--- mouth.py
+++ mouth.py
@@ -65,11 +65,12 @@ class Parser:
             rest=instring
 
         words=rest.split(' ')
+        if '->' in words:
+            self.brain.orig_rule_exp.append(words[words.index('->')-1])
         command=words[0].lower().capitalize()
         for word in reversed(words[1:len(words)]):
             if '&' in word or '|' in word or '!' in word:
-                if command == 'Teach':
-                    self.brain.orig_rule_exp.append(word)
+
                 args.insert(0,self.refine(word,[]))
                 print args
             elif '(' in word and ')' in word:
